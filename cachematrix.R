7c1befa056ba4ad4124f562ee1d6ca8f2c264279
shebinjs <getshebin@gmail.com>
2014-10-23 17:27:32 +0530

    Finished the R code for setting and getting a matrix and its inverse, using cache functionality. UNit testing completed successfully.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file contains two functions. 1)makeCacheMatrix 2)cacheSolve
+# Both the functions together provides the ability to cache a matrix once, set its inverse
+# Then read the matrix and its inverse from cache as required
 
-## Write a short comment describing this function
+# makeCacehMatrix accepts a matrix input and set the four functions to the object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function () x
+    setInv <- function(xInv) inv <<- xInv
+    getInv <- function() inv
+    
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve is used to get a matrix inverse and 
+# if cache has the inverse already, then its taken
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if(!is.null(inv)){
+        message("getting cached data ...")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix)
+    x$setInv(inv)
+    inv
 }
